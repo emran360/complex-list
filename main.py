be6129d256f4599d72df8a9f8278b1279bd56100
emran360 <emran420825@gmail.com>
2022-10-14 21:49:09 +0600

    edit

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import random
+mobile_list = [
+    {'name':'Iphone 22', 'price': 150000},
+    {'name':'Nokia 23', 'price': 50000},
+    {'name':'Huwai 24', 'price': 35000},
+    {'name':'Xiaomi 4', 'price': 39000},
+    {'name':'Samsung 5', 'price': 59000},
+]
+# Iphone 22 Price is 15000 /
 
-# Challenge One
-years_of_service_time = int(input('Enter Your Years of Service Time Now: '))
-salary = int(input('Enter your salary: '))
+for mobile in mobile_list:
+    text = [
+        f'{mobile.get("name")} Price is {mobile.get("price")} BDT.',
+        f'The price of {mobile.get("name")} is {mobile.get("price")} BDT'
+            ]
 
-if years_of_service_time >= 5:
-    print('your salary is', salary, 'and', 'you got bonus', 'now your net bonus amount is', (salary / 100) * 5 + salary)
-else:
-    print('your years of service time is not enough. Try next year')
+    print(random.choice(text))
 
-# Challenge Two
-
-product_purchase_quantity = int(input('Enter the number of product you buy: '))
-per_product_cost = int(input('Enter average per product cost: '))
-customer_total_cost = product_purchase_quantity * per_product_cost
-discount = (customer_total_cost / 100 ) * 10
-if customer_total_cost >1000:
-    print('you got discount', discount, 'TK.', ' Now your total cost is', customer_total_cost - discount, 'TK.' )
-else:
-    print('If you want discount Please next time buy more product')
